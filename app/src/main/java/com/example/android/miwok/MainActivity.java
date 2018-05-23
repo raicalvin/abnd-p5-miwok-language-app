2140255f4e0e3634e07dd6c92a4420568a28e093
Calvin Rai <raicalvin@gmail.com>
2018-05-23 16:35:56 -0700

    Created and wired 4 Views to correspond to each of the categories in the XML layout.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,8 +19,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +31,25 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the view that shows the numbers category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        // Find the view that shows the family category
+        TextView family = (TextView) findViewById(R.id.family);
+
+        // Find the view that shows the colors category
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        // Find the view that shows the phrases category
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
     }
 
     public void openNumbersList(View view) {
         Intent i = new Intent(this, NumbersActivity.class);
         startActivity(i);
     }
+
+
 }
