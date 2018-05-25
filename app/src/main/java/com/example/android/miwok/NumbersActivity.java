1c1e70de4c24df97b3d6d39c73b24d0776b22ef0
Calvin Rai <raicalvin@gmail.com>
2018-05-24 22:14:26 -0700

    Switched from Array to ArrayList for NumbersActivity.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -11,7 +14,7 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         // Create array to store number words
-        String[] words = new String[10];
+        /*String[] words = new String[10];
 
         words[0] = "one";
         words[1] = "two";
@@ -23,6 +26,46 @@ public class NumbersActivity extends AppCompatActivity {
         words[7] = "eight";
         words[8] = "nine";
         words[9] = "ten";
+
+        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
+        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
+        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
+        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
+        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
+        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
+        Log.v("NumbersActivity", "Word at index 0: " + words[8]);
+        Log.v("NumbersActivity", "Word at index 0: " + words[9]); */
+
+        // Now create an ArrayList to store the words
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("size");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Use the LogCat to determine the object at each index
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
+        Log.v("NumbersActivity", "Word at index 2: " + words.get(2));
+        Log.v("NumbersActivity", "Word at index 3: " + words.get(3));
+        Log.v("NumbersActivity", "Word at index 4: " + words.get(4));
+        Log.v("NumbersActivity", "Word at index 5: " + words.get(5));
+        Log.v("NumbersActivity", "Word at index 6: " + words.get(6));
+        Log.v("NumbersActivity", "Word at index 7: " + words.get(7));
+        Log.v("NumbersActivity", "Word at index 8: " + words.get(8));
+        Log.v("NumbersActivity", "Word at index 9: " + words.get(9));
+
+
+
 
     }
 }
