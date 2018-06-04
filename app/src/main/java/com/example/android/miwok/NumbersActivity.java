ca580dfddc4f1525483d03bc669e407c11be6ca9
Calvin Rai <raicalvin@gmail.com>
2018-06-04 16:38:51 -0700

    Created a Word class to store the default and Miwok translation for each word.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,11 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -68,6 +73,48 @@ public class NumbersActivity extends AppCompatActivity {
         Log.v("NumbersActivity", "Word at index 9: " + words.get(9));
         */
 
+        // Create a variable to reference LinearLayout in numbers_activity.xml
+        // LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        // Create a while loop to add in the TextViews
+        /*
+        int i = 0;
+        while (i < words.size()) {
+            TextView textViewToAdd = new TextView(this);
+            textViewToAdd.setText(words.get(i));
+            rootView.addView(textViewToAdd);
+            i++;
+        }
+        */
+
+        /*
+        for (int i = 0; i < words.size(); i++) {
+            TextView textViewToAdd = new TextView(this);
+            textViewToAdd.setText(words.get(i));
+            rootView.addView(textViewToAdd);
+        }
+        */
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        // ArrayAdapter<String> itemsAdapter =
+        //         new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, R.id.list_item, words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+        // GridView gridview = (GridView) findViewById(R.id.gridview);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
 
 
     }
---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,48 @@
+package com.example.android.miwok;
+
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ * Since the class is declared public, other classes can access it, such as the MainActivity.
+ */
+public class Word {
+
+    // Both of the strings below are private, since they are only accessed within the class. Since they are private members, we add an 'm' to their variable name to indicate that. For an example, mDefaultTranslation. The 'm' is added to the start of each variable name.
+
+    /** Default translation for the word */
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    // Now the constructor: takes in two Strings and set the translation words from above. The name has to match the class name exactly and has no return type.
+
+    /**
+     * Create a new Word object
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Get the default translation of the word.
+     * @return Default translation
+     */
+    public String getDefaulTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     * @return Miwok translation
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+}
