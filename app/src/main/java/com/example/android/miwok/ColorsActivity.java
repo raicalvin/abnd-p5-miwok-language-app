9e1763ee64da449db54590904927b016ebf3c01c
Calvin Rai <raicalvin@gmail.com>
2018-06-09 15:31:39 -0700

    Updated ArrayList in ColorsActivity to include image resource ID.

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -17,14 +17,14 @@ public class ColorsActivity extends AppCompatActivity {
         ArrayList<Word> words = new ArrayList<Word>();
 
         // Add every word you want to put into the list as a Word object using the custom-created Word class
-        words.add(new Word("red", "weretti"));
-        words.add(new Word("green", "chokokki"));
-        words.add(new Word("brown", "takaakki"));
-        words.add(new Word("gray", "topoppi"));
-        words.add(new Word("black", "kululli"));
-        words.add(new Word("white", "kelelli"));
-        words.add(new Word("dusty yellow", "topiise"));
-        words.add(new Word("mustard yellow", "chiwiite"));
+        words.add(new Word("red", "weretti", R.drawable.color_red));
+        words.add(new Word("green", "chokokki", R.drawable.color_green));
+        words.add(new Word("brown", "takaakki", R.drawable.color_brown));
+        words.add(new Word("gray", "topoppi", R.drawable.color_gray));
+        words.add(new Word("black", "kululli", R.drawable.color_black));
+        words.add(new Word("white", "kelelli", R.drawable.color_white));
+        words.add(new Word("dusty yellow", "topiise", R.drawable.color_dusty_yellow));
+        words.add(new Word("mustard yellow", "chiwiite", R.drawable.color_mustard_yellow));
 
         WordAdapter adapter = new WordAdapter(this, words);
 
