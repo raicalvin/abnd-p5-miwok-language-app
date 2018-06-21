517f9fc65d76a9366f4242abaf2fb72ed0ec15a1
Calvin Rai <raicalvin@gmail.com>
2018-06-21 10:30:33 -0700

    Added PagerView layout to swipe between 4 categories.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,13 +18,12 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,70 +32,15 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        // Find the view that shows the numbers category
-        final TextView numbers = (TextView) findViewById(R.id.numbers);
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        // Find the view that shows the family category
-        TextView family = (TextView) findViewById(R.id.family);
+        // Create an adapter that knows which fragment should be shown on each page
+        MiwokFragmentPagerAdapter adapter = new MiwokFragmentPagerAdapter(getSupportFragmentManager());
 
-        // Find the view that shows the colors category
-        TextView colors = (TextView) findViewById(R.id.colors);
-
-        // Find the view that shows the phrases category
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-
-        // Set a clickListener on the numbers view
-        numbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create a new intent for open the NumbersActivity
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-                // Start the NumbersActivity
-                startActivity(numbersIntent);
-            }
-        });
-
-        // Set a clickListener on the family view
-        family.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create a new intent for open the FamilyActivity
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-                // Start the FamilyActivity
-                startActivity(familyIntent);
-            }
-        });
-
-        // Set a clickListener on the colors view
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create a new intent for open the ColorsActivity
-                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-                // Start the ColorsActivity
-                startActivity(colorsIntent);
-            }
-        });
-
-        // Set a clickListener on the phrases view
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create a new intent for open the PhrasesActivity
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-                // Start the PhrasesActivity
-                startActivity(phrasesIntent);
-            }
-        });
-
-
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
 
     }
-
-    public void openNumbersList(View view) {
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
-
 
 }
