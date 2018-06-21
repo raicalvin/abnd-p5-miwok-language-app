8dc16ce67cb5a17ae67b13b3371cf16ada6938d9
Calvin Rai <raicalvin@gmail.com>
2018-06-21 12:42:57 -0700

    Added Tabs for navigation in ViewPager for all four fragments.

--- app/src/main/java/com/example/android/miwok/MiwokFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/MiwokFragmentPagerAdapter.java
@@ -1,13 +1,18 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class MiwokFragmentPagerAdapter extends FragmentPagerAdapter {
 
-    public MiwokFragmentPagerAdapter(FragmentManager fm) {
+    /** Context of the app */
+    private Context mContext;
+
+    public MiwokFragmentPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -28,4 +33,25 @@ public class MiwokFragmentPagerAdapter extends FragmentPagerAdapter {
         return 4;
     }
 
+    /**
+     * This method may be called by the ViewPager to obtain a title string
+     * to describe the specified page. This method may return null
+     * indicating no title for this page. The default implementation returns
+     * null.
+     *
+     * @param position The position of the title requested
+     * @return A title for the requested page
+     */
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
+    }
 }
