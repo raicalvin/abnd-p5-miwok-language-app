128934f5fb6bd2b3199e53ff46f34f70674e2d2a
Calvin Rai <raicalvin@gmail.com>
2018-06-21 09:59:55 -0700

    Converted PhrasesActivity to PhrasesFragment.

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,137 +1,16 @@
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-
-import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
-import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
 
 public class PhrasesActivity extends AppCompatActivity {
-
-    MediaPlayer mMediaPlayer;
-
-    /** handles audio focus when playing a sound file */
-    private AudioManager mAudioManager;
-
-    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-        @Override
-        public void onAudioFocusChange(int focusChange) {
-            if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-                // pause playback
-                mMediaPlayer.pause();
-                mMediaPlayer.seekTo(0);
-            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                // audiofocus_gain case means we have regained focus and can resume playback
-                mMediaPlayer.start();
-            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                // we lost audio focus and stop playback and cleanup resources
-                releaseMediaPlayer();
-            }
-        }
-    };
-
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mp) {
-            releaseMediaPlayer();
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-        // Create and setup the {@link AudioManager} to request audio focus
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        // Now create an ArrayList of Word objects. Each element is a Word object
-        final ArrayList<Word> words = new ArrayList<Word>();
-
-        // Add every word you want to put into the list as a Word object using the custom-created Word class
-        words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
-        words.add(new Word("What is your name?", "tinne oyaase'na", R.raw.phrase_what_is_your_name));
-        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
-        words.add(new Word("How are you feeling?", "michekses?", R.raw.phrase_how_are_you_feeling));
-        words.add(new Word("I'm feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
-        words.add(new Word("Are you coming?", "eenes'aa", R.raw.phrase_are_you_coming));
-        words.add(new Word("Yes, I'm coming.", "hee'eenem", R.raw.phrase_yes_im_coming));
-        words.add(new Word("I'm coming.", "eenem", R.raw.phrase_im_coming));
-        words.add(new Word("Let's go.", "yoowutis", R.raw.phrase_lets_go));
-        words.add(new Word("Come here.", "enni'nem", R.raw.phrase_come_here));
-
-        WordAdapter adapter = new WordAdapter(this, words, R.color.category_phrases);
-
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int resource = words.get(position).getRawResourceID();
-
-                // Release the media player if it currently exists because we are about to play a different sound file.
-                releaseMediaPlayer();
-
-                mMediaPlayer = MediaPlayer.create(getApplicationContext(), resource);
-
-                mMediaPlayer.start();
-
-                mMediaPlayer.setOnCompletionListener(mCompletionListener);
-            }
-        });
-
-        AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-            @Override
-            public void onAudioFocusChange(int focusChange) {
-                if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT) {
-                    // pause playback
-                    mMediaPlayer.pause();
-                } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                    // stop playback
-                    mMediaPlayer.stop();
-                    releaseMediaPlayer();
-                } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-                    // lower volume
-                } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                    // resume playback
-                }
-            }
-        };
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new PhrasesFragment())
+                .commit();
     }
-
-    /**
-     * Clean up the media player by releasing its resources
-     */
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a song
-        if (mMediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resources because we no longer need them
-            mMediaPlayer.release();
-
-            // Set the media player back to null. For our code, we've decided that setting the media player to null is an easy wayt o tell that the media player is not configured to play an audio file at the moment
-            mMediaPlayer = null;
-
-            // regardless of whether or not we were granted audio focus, abandon it. This also unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
-            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-        }
-    }
-
-    // Release media resources when user leaves the activity
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-
 }
---
+++ app/src/main/java/com/example/android/miwok/PhrasesFragment.java
@@ -0,0 +1,150 @@
+package com.example.android.miwok;
+
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PhrasesFragment extends Fragment {
+
+    private MediaPlayer mMediaPlayer;
+
+    /** handles audio focus when playing a sound file */
+    private AudioManager mAudioManager;
+
+    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // pause playback
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // audiofocus_gain case means we have regained focus and can resume playback
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // we lost audio focus and stop playback and cleanup resources
+                releaseMediaPlayer();
+            }
+        }
+    };
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
+
+    public PhrasesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        // Create and setup the {@link AudioManager} to request audio focus
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        // Now create an ArrayList of Word objects. Each element is a Word object
+        final ArrayList<Word> words = new ArrayList<Word>();
+
+        // Add every word you want to put into the list as a Word object using the custom-created Word class
+        words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
+        words.add(new Word("What is your name?", "tinne oyaase'na", R.raw.phrase_what_is_your_name));
+        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
+        words.add(new Word("How are you feeling?", "michekses?", R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("I'm feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
+        words.add(new Word("Are you coming?", "eenes'aa", R.raw.phrase_are_you_coming));
+        words.add(new Word("Yes, I'm coming.", "hee'eenem", R.raw.phrase_yes_im_coming));
+        words.add(new Word("I'm coming.", "eenem", R.raw.phrase_im_coming));
+        words.add(new Word("Let's go.", "yoowutis", R.raw.phrase_lets_go));
+        words.add(new Word("Come here.", "enni'nem", R.raw.phrase_come_here));
+
+        WordAdapter adapter = new WordAdapter(getActivity(), words, R.color.category_phrases);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int resource = words.get(position).getRawResourceID();
+
+                // Release the media player if it currently exists because we are about to play a different sound file.
+                releaseMediaPlayer();
+
+                mMediaPlayer = MediaPlayer.create(getActivity(), resource);
+
+                mMediaPlayer.start();
+
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+            }
+        });
+
+        AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+            @Override
+            public void onAudioFocusChange(int focusChange) {
+                if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT) {
+                    // pause playback
+                    mMediaPlayer.pause();
+                } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                    // stop playback
+                    mMediaPlayer.stop();
+                    releaseMediaPlayer();
+                } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                    // lower volume
+                } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                    // resume playback
+                }
+            }
+        };
+
+        return rootView;
+    }
+
+    /**
+     * Clean up the media player by releasing its resources
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a song
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources because we no longer need them
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that setting the media player to null is an easy wayt o tell that the media player is not configured to play an audio file at the moment
+            mMediaPlayer = null;
+
+            // regardless of whether or not we were granted audio focus, abandon it. This also unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+}
